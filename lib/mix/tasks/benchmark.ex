848c1092e0a28580875695f6d4f622e3adfbe168
Christoph Pilka <c.pilka@asconix.com>
2016-09-26 13:59:42 +0200

    Approaching concurrency via Task.async/2 and Task.await/1

--- lib/mix/tasks/benchmark.ex
+++ lib/mix/tasks/benchmark.ex
@@ -6,21 +6,26 @@ defmodule Mix.Tasks.Benchmark do
 
   def run(_) do
     Mix.Task.run "app.start"
-    send_riemann(1_000_000)
+    << a :: 32, b :: 32, c :: 32 >> = :crypto.strong_rand_bytes(12)
+    :rand.seed(:exs1024, {a,b,c})
+    send_riemann(100_000)
   end
 
   defp send_riemann(n) when n <= 1 do
-    create_event |> Riemann.send
+    create_event
+    |> Riemann.send
   end
 
   defp send_riemann(n) do
-    create_event |> Riemann.send
+    create_event
+    |> Enum.map(&Task.async(fn -> Riemann.send(&1) end))
+    |> Enum.map(&Task.await/1)
     send_riemann(n-1)
   end
 
   defp create_event do
-    << a :: 32, b :: 32, c :: 32 >> = :crypto.rand_bytes(12)
-    :rand.seed(:exs1024, {a,b,c})
-    [service: "CPU utilization", metric: :rand.uniform, attributes: [cpu_number: "1"]]   
+    [[service: "CPU utilization",
+     metric: :rand.uniform,
+     attributes: [cpu_number: "1"]]]
   end
 end
