d0db715ed4353e1da8ae209f318acc6199f4d36e
Vincent Ambo <tazjin@gmail.com>
2016-09-26 14:43:27 +0200

    feat benchmark: OTP app with integrated counter
    Turns the benchmark into a constantly running OTP app that prints a req/sec average every 5 seconds.

---
+++ lib/worker.ex
@@ -0,0 +1,24 @@
+defmodule RiemannBenchmarkElixir.Worker do
+  alias RiemannBenchmarkElixir.Counter
+
+  def start_link(n) do
+    pid = spawn(fn() -> benchmark(n) end)
+    {:ok, pid}
+  end
+
+  defp benchmark(n) do
+    Stream.repeatedly(&create_event/0)
+    |> Stream.take(n)
+    |> Stream.map(&Task.async(fn ->
+          Riemann.send(&1)
+          Counter.inc
+        end))
+    |> Stream.run()
+  end
+
+  def create_event do
+    [service: "CPU utilization",
+     metric: :rand.uniform,
+     attributes: [cpu_number: "1"]]
+  end
+end
